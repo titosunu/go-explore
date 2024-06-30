6bee7d3c5b05ee145d855ba0be818ed00b4904c7
titosunu <titosunu2001@gmail.com>
2024-06-30 14:56:11 +0700

    goroutine

--- goroutine/channel_test.go
+++ goroutine/channel_test.go
@@ -2,6 +2,7 @@ package goroutine
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -20,4 +21,104 @@ func TestChannel(t *testing.T) {
 	defer close(channel)
 
 	time.Sleep(5 * time.Second)
+}
+
+func GiveResponse(channel chan string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Pratito Sunu Darmalaksana"
+}
+
+func TestChannelParameter(t *testing.T) {
+	channel := make(chan string)
+
+	go GiveResponse(channel)
+
+	data := <- channel
+	fmt.Println(data)
+	close(channel)
+}
+
+func OnlyIn(channel chan <- string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Pratito Sunu Darmalaksana"
+}
+
+func OnlyOut(channel <- chan string) {
+	data := <- channel
+	fmt.Println(data)
+}
+
+func TestInOutChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+	go OnlyIn(channel)
+	go OnlyOut(channel)
+	time.Sleep(5 * time.Second)
+}
+
+func TestBufferedChannel(t *testing.T) {
+	channel := make(chan string, 3)
+	defer close(channel)
+
+	go func() {
+		channel <- "Pratito"
+		channel <- "Sunu"
+		channel <- "Darmalaksana"
+	}()
+
+	go func(){
+		fmt.Println(<- channel)
+		fmt.Println(<- channel)
+		fmt.Println(<- channel)
+	}()
+
+	fmt.Println("Success")
+}
+
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func ()  {
+		for i := 0; i < 10; i++ {
+			channel <- "Perulangan ke " + strconv.Itoa(i)
+		}
+		close(channel)
+	}()
+
+	for data := range channel {
+		fmt.Println("Menerima Data", data)
+	}
+
+	fmt.Println("selesai")
+}
+
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveResponse(channel1)
+	go GiveResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+
+		case data := <- channel1:
+			fmt.Println("Data dari channel 1 ", data)
+			counter++
+		case data := <- channel2:
+			fmt.Println("Data dari channel 2 ", data)
+			counter++
+		default:
+			fmt.Println("Menunggu data")
+		}
+
+		if counter == 2 {
+			break
+		}
+
+	}
+
 }
